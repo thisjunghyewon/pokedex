928f4a63f16a07cbf64af0a40a7fa43adac5100f
thisjunghyewon <this.junghyewon@gmail.com>
2023-06-22 16:53:33 +0200

    created
    Variable array pokemonList

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        type: ['GRASS','POISON']
+    },
+    {
+        name: 'Jigglypuff',
+        height: 0.5,
+        type: ['FAIRY','NORMAL']
+    },
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        type: 'ELECTRIC'
+    }
+];
