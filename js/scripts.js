3d4308c6426507cba0feb79351a211c9307feed0
thisjunghyewon <this.junghyewon@gmail.com>
2023-06-23 15:48:29 +0200

    feat
    Add for loop to check Pokemon's name  and height, conditional clause added when if the height is above a certain value

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,18 @@ let pokemonList = [
         type: ['ELECTRIC']
     }
 ];
+
+for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].height >= 7) {
+        document.write(
+            pokemonList[i].name + ` (Height: ${pokemonList[i].height})` + ' - Wow, that\'s big!'
+        );
+    }
+    else {
+        document.write(
+            pokemonList[i].name + ` (Height: ${pokemonList[i].height})`
+        );
+    }
+    // adds a row space in-between
+    document.write('<br> <br>')
+}
