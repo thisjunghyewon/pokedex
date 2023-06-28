8a949f27191e374dfff161d939ed9643bea5e502
thisjunghyewon <this.junghyewon@gmail.com>
2023-06-28 17:44:30 +0200

    feat
    Wrap the pokemonList array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,23 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 7,
-        type: ['GRASS','POISON']
-    },
-    {
-        name: 'Jigglypuff',
-        height: 0.5,
-        type: ['FAIRY','NORMAL']
-    },
-    {
-        name: 'Pikachu',
-        height: 0.4,
-        type: ['ELECTRIC']
-    }
-];
+(function () {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 7,
+            type: ['GRASS','POISON']
+        },
+        {
+            name: 'Jigglypuff',
+            height: 0.5,
+            type: ['FAIRY','NORMAL']
+        },
+        {
+            name: 'Pikachu',
+            height: 0.4,
+            type: ['ELECTRIC']
+        }
+    ];
+})();
+
 
 
 function myLoopFunction(list) {
