a47058f2532428418d7460276fa500fc7e8e28f1
thisjunghyewon <this.junghyewon@gmail.com>
2023-07-05 19:23:18 +0200

    Add
    function loadDetails()

--- js/scripts.js
+++ js/scripts.js
@@ -51,6 +51,23 @@ let pokemonRepository = (function () {
       });
   }
 
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        // Now we add the details to the item
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+
   return {
     add: add,
     getAll: getAll,
