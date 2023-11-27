628f40f121c7fdadac71fb9072608e6ebd3b1229
thisjunghyewon <this.junghyewon@gmail.com>
2023-11-27 17:50:16 +0100

    Fix & Note commit

--- src/scripts.js
+++ src/scripts.js
@@ -39,6 +39,7 @@ let pokemonRepository = (function () {
       })
       .then((json) => {
         json.results.forEach((item) => {
+          // JSON으로 변환된 정보를 처리하고, 각 포켓몬에 대한 정보를 추출하고 새로운 포켓몬 객체를 만듭니다. 그런 다음 add 함수를 사용하여 이러한 새로운 포켓몬을 목록에 추가합니다.
           let pokemon = {
             name: item.name,
             detailsUrl: item.url,
@@ -52,8 +53,9 @@ let pokemonRepository = (function () {
       });
   }
 
+  //loadDetails 함수는 loadList에서 만들어진 목록의 각 포켓몬에 대해 호출됩니다. 이렇게 하면 목록에 있는 포켓몬들이 더 많은 정보를 가지게 됩니다.
   function loadDetails(pokemon) {
-    let url = pokemon.detailsUrl;
+    let url = pokemon.detailsUrl; //use the detailsUrl property to load the detailed data for a given Pokémon.
     return fetch(url)
       .then((response) => {
         return response.json();
@@ -68,6 +70,7 @@ let pokemonRepository = (function () {
       });
   }
 
+  //when a user clicks on a Pokémon
   function showDetails(pokemon) {
     loadDetails(pokemon)
       .then((details) => {
@@ -79,6 +82,7 @@ let pokemonRepository = (function () {
       });
   }
 
+  //Modal shows these information
   function showModal(pokemonName, pokemonHeight, pokemonImage) {
     let titleElement = document.querySelector(".modal-title");
     titleElement.innerText = pokemonName.toUpperCase();
@@ -96,12 +100,14 @@ let pokemonRepository = (function () {
     modalContainer.classList.remove("isVisible");
   }
 
+  // "Esc" 키를 눌렀을 때 모달(팝업 창)이 열려 있다면 모달을 닫는 역할
   window.addEventListener("keydown", (e) => {
     if (e.key === "Escape" && modalContainer.classList.contains("isVisible")) {
       hideModal();
     }
   });
 
+  //컴퓨터 화면에 무엇인가를 클릭했을 때, 그 클릭한 곳을 확인하고, 만약 그게 팝업 창의 바깥 부분이면, 그 팝업 창을 닫아준다.
   modalContainer.addEventListener("click", (e) => {
     // Since this is also triggered when clicking INSIDE the modal container,
     // We only want to close if the user clicks directly on the overlay
